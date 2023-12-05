c637102a5e32172cacefc32b33b10b15b8160b48
mahdi <mahdimamashli1383@gmail.com>
2023-12-05 10:52:03 +0330

    create route test

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const http = require('http');
+const http = require("http");
+const { url } = require("inspector");
 const server = http.createServer((req,res)=>{
-    console.log(req.url , req.method , req.headers);
-
-
     res.headers('Content-Type', "text/html")
-    res.write("hi this is test")
-    res.end()
-}).listen(3000)
+    console.log(req.url , req.method , req.headers);
+    if (url === "/") {
+        res.write("hi this is test for url => /")
+        res.end()
+    }else if (url === '/test'){
+        res.write("hi this is tes fot url => /test")
+        res.end()
+    }else{
+        res.write("hi this is test")
+        res.end()
+    }
+
+})
+server.listen(3000)
