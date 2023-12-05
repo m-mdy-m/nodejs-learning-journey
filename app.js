f762ed59d9f04f200ffdcfb439c40064952dcbb9
mahdi <mahdimamashli1383@gmail.com>
2023-12-05 16:28:20 +0330

    create and run server with expressjs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require("http");
+// const http = require("http");
 const express = require("express");
 const app = express();
 // const router = require("./routes.js")
@@ -10,7 +10,10 @@ app.use((req, res, next) => {
   console.log("next =>", next); 
   next();
 });
-const server = http.createServer(app);
-server.listen(80, () => {
-  console.log("run server port 80");
-});
+// const server = http.createServer(app);
+// server.listen(80, () => {
+//   console.log("run server port 80");
+// });
+app.listen(3000,()=>{
+    console.log('run server port 3000');
+})
