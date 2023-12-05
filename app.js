66c1e8ffc4442d69e3e658a6905e055c5bc32cd7
mahdi <mahdimamashli1383@gmail.com>
2023-12-05 11:09:23 +0330

    redirect test with work for fs

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const http = require("http");
-const { url } = require("inspector");
-const server = http.createServer((req,res)=>{
-    res.headers('Content-Type', "text/html")
-    console.log(req.url , req.method , req.headers);
-    if (url === "/") {
-        res.write("hi this is test for url => /")
-        res.end()
-    }else if (url === '/test'){
-        res.write("hi this is tes fot url => /test")
-        res.end()
-    }else{
-        res.write("hi this is test")
-        res.end()
-    }
+const fs = require('fs')
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method
+  res.setHeader("Content-Type", "text/html"); 
 
-})
-server.listen(3000)
+  if (url === "/") {
+    res.write("hi this is test for url => /");
+    res.end();
+  } else if (url === "/test") {
+    res.write("hi this is test for url => /test");
+    res.end();
+
+  } else if (url=== "/message" && method === 'POST'){
+
+  } else {
+    res.write("hi this is test");
+    res.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log('server on 3000 ');
+});
