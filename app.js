3d25853da76845d21ee36fef12c3f82cdebe4a34
mahdi <mahdimamashli1383@gmail.com>
2023-12-05 15:48:37 +0330

    run server with expressjs

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const express = require("express")
+const app = express()
 // const router = require("./routes.js")
 // console.log(router.someText);
 // const server = http.createServer(router.handlre )
-
+const server = http.createServer(app)
 server.listen(80, () => {
   console.log("run server port 80");
 });
