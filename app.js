3a58391b4d683af799377a31d8a76b903aba6565
mahdi <mahdimamashli1383@gmail.com>
2023-12-05 16:39:30 +0330

    Finalize simple Express server setup with demonstration middleware

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-// const http = require("http");
+// Import the express module
 const express = require("express");
-const app = express();
-// const router = require("./routes.js")
-// console.log(router.someText);
-// const server = http.createServer(router.handlre )
 
+// Create an Express application
+const app = express();
+
+// This is middleware that processes every request that comes in.
 app.use((req, res, next) => {
+  // Send a response with the text "hi this is test" to the client
   res.send("hi this is test");
-  console.log("next =>", next); 
+  
+  // Log the 'next' function to the console for demonstration purposes
+  console.log("next =>", next);
+
+  // Proceed to the next middleware (if there is none, this does nothing)
   next();
 });
-// const server = http.createServer(app);
-// server.listen(80, () => {
-//   console.log("run server port 80");
-// });
+
+// Start the Express application server on port 3000 and log a message to the console.
 app.listen(3000,()=>{
-    console.log('run server port 3000');
-})
+    console.log('Server running on port 3000');
+});
