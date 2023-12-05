622c546ff17df774df30509a087121f142718815
mahdi <mahdimamashli1383@gmail.com>
2023-12-05 10:41:02 +0330

    set headers and type text for test

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    
+    console.log(req.url , req.method , req.headers);
+
+
+    res.headers('Content-Type', "text/html")
+    res.write("hi this is test")
+    res.end()
 }).listen(3000)
