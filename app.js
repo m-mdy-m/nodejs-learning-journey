95449216bb6cc7316d22a242129a6a89f1c0dc8b
mahdi <mahdimamashli1383@gmail.com>
2023-12-05 13:33:32 +0330

    create router file

--- app.js
+++ app.js
@@ -1,37 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const router = require("./routes.js")
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  res.setHeader("Content-Type", "text/html");
-  if (url === "/") {
-    res.write(`
-    <form action="/msg" method="POST">
-        <input type="text" name="message" placeholder="type text">
-        <button type="submit">Send</button>
-    </form>
-`);
-    return res.end();
-  } else if (url === "/msg" && method === "POST") {
-    const text = [];
-    req.on("data", (chunk) => {
-      text.push(chunk);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(text).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("text.txt", message, (err) => {
-        if (err) {
-          res.statusCode = 500;
-          return res.end("Error writing to file");
-        }
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        res.end();
-      });
-    });
-  }
+    
 });
 server.listen(80, () => {
   console.log("run server port 80");
---
+++ routes.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const reqHand = (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write(`
+    <form action="/msg" method="POST">
+        <input type="text" name="message" placeholder="type text">
+        <button type="submit">Send</button>
+    </form>
+`);
+    return res.end();
+  } else if (url === "/msg" && method === "POST") {
+    const text = [];
+    req.on("data", (chunk) => {
+      text.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(text).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("text.txt", message, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          return res.end("Error writing to file");
+        }
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+      });
+    });
+  }
+};
+module.exports = reqHand
