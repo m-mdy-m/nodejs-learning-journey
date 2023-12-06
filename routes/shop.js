70eb36d1889cc9141b8a4589c8cfddc9cc47a09b
mahdi <mahdimamashli1383@gmail.com>
2023-12-06 22:29:13 +0330

    Working with pug and displaying the imported product

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,15 @@
 // shop.js
 const path = require("path");
-const routeDir = require("../util/path.js")
+const routeDir = require("../util/path.js");
 const express = require("express");
 const router = express.Router();
-const admin = require("./admin.js")
+const admin = require("./admin.js");
 router.get("/", (req, res, next) => {
-  console.log("shop =>",admin.product);
-  res.sendFile(path.join(routeDir, 'views', 'shop.html'));
+	const product = admin.product;
+	// console.log("shop =>",admin.product);
+	// res.sendFile(path.join(routeDir, 'views', 'shop.html'));
+	console.log("products =>", product);
+	res.render("shop", { prods: product, docTitle: "shop" });
 });
 
-module.exports = router; 
+module.exports = router;
