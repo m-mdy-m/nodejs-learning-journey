3854b02b530adb5f6bd31fb42ce32ec1391c2094
mahdi <mahdimamashli1383@gmail.com>
2023-12-06 22:49:15 +0330

    dynamic project title page with pug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/admin", AdminRouter.router);
 app.use(ShopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(routeDir, "views", "404.html"));
+  res.status(404).render('404',{pageTitle : "pages 404"})
 });
 
 app.listen(3000, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const product = [];
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(routeDir, "views", "add-product.html"));
+  res.render('add-product',{pageTitle:"add products"})
 });
 
 router.post("/product", (req, res, next) => {
