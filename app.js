e6137f508bcab8ad92fe0f84dad899649ff159a9
mahdi <mahdimamashli1383@gmail.com>
2023-12-06 13:08:20 +0330

    add page 404

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const ShopRouter = require("./routes/shop.js")
 
 app.use(bodyParser.urlencoded({extended :false}));
 app.use(AdminRouter)
-
 app.use(ShopRouter)
+
+app.use((req,res,next)=>{
+  res.status(404).send("<h1 style=color:red;>Page Not Found 404</h1>")
+})
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
