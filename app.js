b31489290084d29b63f0255af3f13be8b563aace
mahdi <mahdimamashli1383@gmail.com>
2023-12-06 21:47:13 +0330

    add pug file

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 // Import required core modules
 const path = require("path");
-const routeDir = require("./util/path.js")
+const routeDir = require("./util/path.js");
 const express = require("express");
 
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.set("view engine", "pug");
+app.set("views", "views");
 const AdminRouter = require("./routes/admin.js");
 const ShopRouter = require("./routes/shop.js");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(routeDir,'public')))
+app.use(express.static(path.join(routeDir, "public")));
 app.use("/admin", AdminRouter.router);
 
 app.use(ShopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(routeDir,"views","404.html"));
+  res.status(404).sendFile(path.join(routeDir, "views", "404.html"));
 });
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000"); 
+  console.log("Server running on port 3000");
 });
