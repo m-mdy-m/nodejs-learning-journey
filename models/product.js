4dc6d3ce03bcca9114f8a4dd81268be9ac7ab253
mahdi <mahdimamashli1383@gmail.com>
2023-12-10 22:33:54 +0330

    create products id in module

--- models/product.js
+++ models/product.js
@@ -23,27 +23,7 @@ module.exports = class Product {
 		this.price = price
 	}
 	save() {
-		// Products.push(this);
-		// const pth = path.join(
-		// 	path.dirname(process.mainModule.filename),
-		// 	"data",
-		// 	"products.json"
-		// );
-		// fs.readFile(pth, (err, data) => {
-		// 	let products = [];
-		// 	if (!err) {
-		// 		products = JSON.parse(data);
-		// 		console.log("pro =>", products);
-		// 	}
-		// 	products.push(this);
-		// 	fs.writeFile(pth, JSON.stringify(products), err => {
-		// 		console.log("err file", err);
-		// 	});
-		// });
-
-		// =====
-		// Better Code =>
-		// =====
+		this.id = Math.random().toString()
 		getProductFormFile(pro => {
 			pro.push(this);
 			fs.writeFile(pth, JSON.stringify(pro), err => {
