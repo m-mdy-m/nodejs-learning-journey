6f52373676be73c8a6dcd66a801a4aad39adcd05
mahdi <mahdimamashli1383@gmail.com>
2023-12-14 11:53:36 +0330

    Add database connection and query to the app.js file.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const controllers404 = require("./controllers/error.js")
+const controllers404 = require("./controllers/error.js");
+const db = require("./util/database.js");
 app.set("view engine", "ejs");
 app.set("views", "views");
 const adminRoutes = require("./routes/admin.js");
@@ -13,9 +14,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 
+db.execute("SELECT * FROM products")
+	.then(result => {
+		console.log(result[0], result[1]);
+	})
+	.catch(err => {
+		console.log(err);
+	});
+
 app.use(ShopRouter);
 
-app.use( controllers404.Error404);
+app.use(controllers404.Error404);
 
 app.listen(3000, () => {
 	console.log("Server running on port 3000");
