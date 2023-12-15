614650bd140dc40bec2840413572ead7e07967c7
mahdi <mahdimamashli1383@gmail.com>
2023-12-15 10:09:09 +0330

    Replace MySQL with Sequelize.

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
 
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user:'root',
+//     database:'node-complete',
+//     password:'mdy_mmshly13831922'
+// })
+// module.exports = pool.promise()
+// =======================
+// Sequelize
+// =======================
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'mdy_mmshly13831922'
-})
-module.exports = pool.promise()
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node-complete", "root", "mdy_mmshly13831922", {
+	dialect: "mysql",
+    host: "localhost"
+});
+module.exports = sequelize
