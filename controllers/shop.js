c93516106f8a504ff91077c6e3c6480dbf3b8946
mahdi <mahdimamashli1383@gmail.com>
2023-12-15 23:26:36 +0330

    Add order details to the orders page and redirect after placing an order

--- controllers/shop.js
+++ controllers/shop.js
@@ -156,16 +156,18 @@ exports.postCartDeleteProduct = (req, res, next) => {
 		});
 };
 exports.postOrder = (req, res, next) => {
+	let fetchedCart;
 	req.user
 		.getCart()
 		.then(cart => {
+			fetchedCart = cart;
 			return cart.getProducts();
 		})
 		.then(products => {
 			return req.user
 				.createOrder()
 				.then(order => {
-					order.addProduct(
+					return order.addProduct(
 						products.map(product => {
 							product.orderItem = {
 								quantity: product.cartItem.quantity,
@@ -178,15 +180,29 @@ exports.postOrder = (req, res, next) => {
 					console.log(err);
 				});
 		})
+		.then(result => {
+			return fetchedCart.setProducts(null);
+		})
+		.then(result => {
+			res.redirect("/orders");
+		})
 		.catch(err => {
 			console.log(err);
 		});
 };
 exports.getOrders = (req, res, next) => {
-	res.render("shop/orders", {
-		path: "/orders",
-		pageTitle: "Your Orders",
-	});
+	req.user
+		.getOrders({ include: ["products"] })
+		.then(orders => {
+			res.render("shop/orders", {
+				path: "/orders",
+				pageTitle: "Your Orders",
+				orders,
+			});
+		})
+		.catch(err => {
+			console.log(err);
+		});
 };
 
 exports.getCheckout = (req, res, next) => {
