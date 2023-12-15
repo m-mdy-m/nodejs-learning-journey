cb7c171e80ce6272b038734419f12c01cf374d69
mahdi <mahdimamashli1383@gmail.com>
2023-12-15 11:17:45 +0330

    Refactor code to use async/await instead of callbacks.

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,24 +2,11 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-	Product.findAll().then(products =>{
-		res.render("shop/product-list", {
-			prods: products,
-			pageTitle: "All Products",
-			path: "/products",
-		});
-	}).catch(err =>{
-		console.log(err);
-	})
-};
-
-exports.getProduct = (req, res, next) => {
-	const prodId = req.params.productId;
-	Product.findById(prodId)
-		.then(product => {
-			res.render("shop/product-detail", {
-				product: product[0],
-				pageTitle: product.title,
+	Product.findAll()
+		.then(products => {
+			res.render("shop/product-list", {
+				prods: products,
+				pageTitle: "All Products",
 				path: "/products",
 			});
 		})
@@ -28,16 +15,47 @@ exports.getProduct = (req, res, next) => {
 		});
 };
 
+exports.getProduct = (req, res, next) => {
+	const prodId = req.params.productId;
+	Product.findAll({ WHERE: { id: prodId } })
+		.then(product => {
+			console.log('products =>', product);
+			if (product) {
+				res.render("shop/product-detail", {
+					product: product[0],
+					pageTitle: product.title,
+					path: "/products",
+				});
+			}
+		})
+		.catch();
+	// Product.findByPk(prodId)
+	// 	.then(product => {
+	// 		if (product) {
+	// 			res.render("shop/product-detail", {
+	// 				product: product,
+	// 				pageTitle: product.title,
+	// 				path: "/products",
+	// 			});
+	// 		}
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err);
+	// 	});
+};
+
 exports.getIndex = (req, res, next) => {
-	Product.findAll().then(products =>{
-		res.render("shop/index", {
-			prods: products,
-			pageTitle: "Shop",
-			path: "/",
+	Product.findAll()
+		.then(products => {
+			res.render("shop/index", {
+				prods: products,
+				pageTitle: "Shop",
+				path: "/",
+			});
+		})
+		.catch(err => {
+			console.log(err);
 		});
-	}).catch(err =>{
-		console.log(err);
-	})
 };
 
 exports.getCart = (req, res, next) => {
