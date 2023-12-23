1c981b04248d4168982de33dea3ebc4d5f1128eb
mahdi <mahdimamashli1383@gmail.com>
2023-12-23 23:17:34 +0330

    Add mongoose schema for user model.

--- models/user.js
+++ models/user.js
@@ -1,3 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	cart: {
+		items: [
+			{
+				productId: { type: Schema.Types.ObjectId },
+				quantity: { type: Number, required: true },
+			},
+		],
+	},
+});
 // const mongodb = require("mongodb");
 // const getDb = require("../util/database").getDb;
 // const Sequelize = require("sequelize");
