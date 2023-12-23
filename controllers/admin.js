8edfca56d3e89ea57fdf9664a6c65b271e8b4781
mahdi <mahdimamashli1383@gmail.com>
2023-12-23 22:45:06 +0330

    Update admin routes and controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -28,20 +28,6 @@ exports.postAddProduct = async (req, res, next) => {
 		console.log("Cannot create product =>", err);
 		res.status(500).send(err); // Send an error response
 	}
-
-	// Product.create({
-	// 	title,
-	// 	price,
-	// 	description,
-	// 	imageUrl,
-	// })
-	// 	.then(result => {
-	// 		console.log("created Product");
-	// 		res.redirect("/");
-	// 	})
-	// 	.catch(err => {
-	// 		console.log(err);
-	// 	});
 };
 
 exports.getEditProduct = async (req, res, next) => {
@@ -64,22 +50,6 @@ exports.getEditProduct = async (req, res, next) => {
 	} catch (err) {
 		console.log(err);
 	}
-	// req.user
-	// 	.getProducts({ WHERE: { id: prodId } })
-	// 	// Product.findByPk(prodId)
-	// 	.then(products => {
-	// 		const product = products[0];
-	// 		if (!product) {
-	// 			return res.redirect("/");
-	// 		}
-	// 		res.render("admin/edit-product", {
-	// 			pageTitle: "Edit Product",
-	// 			path: "/admin/edit-product",
-	// 			editing: editMode,
-	// 			product: product,
-	// 		});
-	// 	})
-	// 	.catch();
 };
 
 exports.postEditProduct = async (req, res, next) => {
@@ -103,26 +73,11 @@ exports.postEditProduct = async (req, res, next) => {
 	} catch (err) {
 		console.log(err);
 	}
-	// Product.findByPk(prodId)
-	// 	.then(products => {
-	// 		products.title = updatedTitle;
-	// 		products.price = updatedPrice;
-	// 		products.imageUrl = updatedImageUrl;
-	// 		products.description = updatedDesc;
-	// 		return products.save();
-	// 	})
-	// 	.then(result => {
-	// 		console.log("update products");
-	// 		res.redirect("/admin/products");
-	// 	})
-	// 	.catch(err => {
-	// 		console.log(err);
-	// 	});
 };
 
 exports.getProducts = async (req, res, next) => {
 	try {
-		const products = await Product.fetchAll();
+		const products = await Product.find();
 		res.render("admin/products", {
 			prods: products,
 			pageTitle: "Admin Products",
@@ -131,20 +86,6 @@ exports.getProducts = async (req, res, next) => {
 	} catch (e) {
 		console.log("e =>", e);
 	}
-
-	// req.user
-	// 	.getProducts()
-	// 	// Product.findAll()
-	// 	.then(products => {
-	// 		res.render("admin/products", {
-	// 			prods: products,
-	// 			pageTitle: "Admin Products",
-	// 			path: "/admin/products",
-	// 		});
-	// 	})
-	// 	.catch(err => {
-	// 		console.log(err);
-	// 	});
 };
 
 exports.postDeleteProduct = async (req, res, next) => {
@@ -152,17 +93,4 @@ exports.postDeleteProduct = async (req, res, next) => {
 	await Product.deleteById(prodId);
 	console.log("DESTROYED PRODUCT ");
 	res.redirect("/admin/products");
-
-	// Product.findByPk(prodId)
-	// 	.then(products => {
-	// 		return products.destroy();
-	// 	})
-	// 	.then(result => {
-	// 		console.log("delete users");
-	// 		res.redirect("/admin/products");
-	// 	})
-	// 	.catch(err => {
-	// 		console.log(err);
-	// 	});
-	// res.redirect("/admin/products");
 };
