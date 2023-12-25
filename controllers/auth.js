7f7527c285f659d938c086662ba5bd4facd66119
mahdi <mahdimamashli1383@gmail.com>
2023-12-25 23:07:08 +0330

    Add login and logout functionality. (generated by blackbox.ai )

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
 	// const cookieArr = req.get("Cookie").split(",");
 	// let isLoggedIn = false;
@@ -14,11 +16,18 @@ exports.getLogin = (req, res, next) => {
 		isAuthenticated: false,
 	});
 };
-exports.postLogin = (req, res, next) => {
+exports.postLogin = async (req, res, next) => {
 	// req.igLoggedIn = true
 	// ** درواقع ما وقتی اینکار میکنیم با درتسور ریدایرکت این اطعلاعات به پایان مرسه و میمیره و دروافع انگار هیچ چیزی نیست که در اینجا کوکی هابه کمک ما می ایند
 	// ست کردن کوکی ها =>
 	// res.setHeader("Set-Cookie", "loggedIn=true;"); // بجای اینکه از کوکی ها استفاده کنیم میتونیم از سشن ها استفاده کنیم
-	req.session.isLoggedIn = true
+	const user = await User.findById("65873ba802bcb4165b0167a6");
+	req.session.isLoggedIn = true;
+	req.session.user = user;
 	res.redirect("/");
+};
+exports.postLogout = async (req, res, next) => {
+	req.session.destroy( ()=>{
+		res.redirect("/");
+	})
 };
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,5 @@ const router = express.Router();
 
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
+router.post('/logout', authController.postLogout)
 module.exports = router
