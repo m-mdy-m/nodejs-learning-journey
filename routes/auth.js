dab9ac4f0d7319b3d9a717b3ff992d89ebe44c44
mahdi <mahdimamashli1383@gmail.com>
2023-12-25 18:44:41 +0330

    Replace the login route handler with a call to the getLogin function in the auth controller.

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,8 @@
 
 const express = require("express");
-
+const authController = require('../controllers/auth')
 
 const router = express.Router();
 
-router.get('/login', (req,res,nxt)=>{
-    
-})
+router.get('/login', authController.getLogin)
 module.exports = router
