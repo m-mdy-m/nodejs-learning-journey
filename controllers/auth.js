cc780a04171f795a0fd1c331327d7906d2cd2bd3
mahdi <mahdimamashli1383@gmail.com>
2023-12-25 20:50:11 +0330

    Set a cookie to store login state

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,17 @@
 exports.getLogin = (req, res, next) => {
-	res.render("auth/login", {
-		path: "/login",
-		pageTitle: "Login",
-		isAuthenticated: req.igLoggedIn,
-		
+	const cookieArr = req.get("Cookie").split(",");
+	let isLoggedIn = false;
+	cookieArr.forEach(cookies => {
+		let [key, value] = cookies.split("=");
+		if (value === "true") {
+			isLoggedIn = value;
+		}
 	});
 };
 exports.postLogin = (req, res, next) => {
 	// req.igLoggedIn = true
-	 // ** درواقع ما وقتی اینکار میکنیم با درتسور ریدایرکت این اطعلاعات به پایان مرسه و میمیره و دروافع انگار هیچ چیزی نیست که در اینجا کوکی هابه کمک ما می ایند
-	 // ست کردن کوکی ها =>
-	 res.setHeader("Set-Cookie", 'loggedIn=true')
-	res.redirect('/')
+	// ** درواقع ما وقتی اینکار میکنیم با درتسور ریدایرکت این اطعلاعات به پایان مرسه و میمیره و دروافع انگار هیچ چیزی نیست که در اینجا کوکی هابه کمک ما می ایند
+	// ست کردن کوکی ها =>
+	res.setHeader("Set-Cookie", "loggedIn=true");
+	res.redirect("/");
 };
