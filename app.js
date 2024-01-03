2d333191bf85cb5e70bceb61e07b3be5f5460066
mahdi <mahdimamashli1383@gmail.com>
2024-01-03 18:24:43 +0330

    Changed the filename format for uploaded images to include a timestamp.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const fileStorage = multer.diskStorage({
 		cb(null, "images");
 	},
 	filename: (req, file, cb) => {
-		cb(null, file.filename + "-" + file.originalname);
+		cb(null, new Date().toISOString() + "-" + file.originalname);
 	},
 });
 app.set("view engine", "ejs");
