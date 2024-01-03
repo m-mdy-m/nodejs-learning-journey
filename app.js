8a600024a0aea2aa0855f8cc8293053491d73c99
mahdi <mahdimamashli1383@gmail.com>
2024-01-03 18:30:01 +0330

    Add file type filter for multer image upload.

--- app.js
+++ app.js
@@ -29,6 +29,17 @@ const fileStorage = multer.diskStorage({
 		cb(null, new Date().toISOString() + "-" + file.originalname);
 	},
 });
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jpeg"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
 app.set("view engine", "ejs");
 app.set("views", "views");
 const adminRoutes = require("./routes/admin.js");
@@ -39,6 +50,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
 	multer({
 		storage: fileStorage,
+		fileFilter: fileFilter,
 	}).single("image")
 );
 // app.use(multer({
