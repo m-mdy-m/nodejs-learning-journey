c5b96f3c635793480b5665c62814d31e2af17cbb
mahdi <mahdimamashli1383@gmail.com>
2024-01-04 00:17:32 +0330

    Update dependencies and add pdfkit

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 const Order = require("../models/order");
 const fs = require("fs");
 const path = require("path");
+const pdfkit = require('pdf')
 exports.getProducts = async (req, res, next) => {
 	const products = Product.find();
 	try {
