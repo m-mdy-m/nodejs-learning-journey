3c0684e4122ea8cbf2c731c49115fa16175c165e
mahdi <mahdimamashli1383@gmail.com>
2024-01-07 12:32:50 +0330

    Changed the HTTP method for deleting a product from POST to DELETE.

--- routes/admin.js
+++ routes/admin.js
@@ -40,5 +40,5 @@ router.post(
 	adminController.postEditProduct
 );
 
-router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+router.delete("/product/:productId", isAuth, adminController.postDeleteProduct);
 module.exports = router;
