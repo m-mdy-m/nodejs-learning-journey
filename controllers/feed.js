89a86648f761e2924ed241b43e77a8c8db93e4d5
mahdi <mahdimamashli1383@gmail.com>
2024-01-12 17:45:34 +0330

    Add create post endpoint

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,3 +3,12 @@ exports.getPosts = (req, res, nxt) => {
     .status(200)
     .json({ posts: [{ title: "POSTS", content: "THIS IS CONTENT POSTS" }] });
 };
+exports.createPost = (req, res, nxt) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: "Post create",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
