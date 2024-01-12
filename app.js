cc32136eb20c265500639d24471f56f84f5370e9
mahdi <mahdimamashli1383@gmail.com>
2024-01-12 18:42:12 +0330

    Added CORS headers to allow cross-origin requests.

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express()
 const feedRoute = require('./routes/feed')
 // app.use(bodyParser.urlencoded()) // x-www-form-url => <form> -> درواقع این برای فرم هاست و ما فرمی نداریم برای restApi
 app.use(bodyParser.json()) /// application/json 
+
+app.use((req,res,nxt)=>{
+    res.setHeader("Access-Control-Allow-Origin", '*') // یعنی هرکسی از هرسایتی میتون به محتوای ما دسترسی داشته باشد
+    res.setHeader("Access-Control-Allow-Methods",'OPTIONS,GET,POST,PUT,PATCH,DELETE') // میتونند با این متد ها استفاده کنند
+    res.setHeader("Access-Control-Allow-Headers", 'Content-Type, Authorization') 
+    nxt()
+})
+
 app.use(feedRoute)
 app.listen(3000, ()=>{
     console.log('run server on port 3000')
