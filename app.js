77ed0169ddae32e270c0ec23128bcff09b0ddf26
mahdi <mahdimamashli1383@gmail.com>
2024-01-12 17:32:22 +0330

    Add JSON body parsing middleware to the Express app.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express =require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const feedRoute = require('./routes/feed')
+// app.use(bodyParser.urlencoded()) // x-www-form-url => <form> -> درواقع این برای فرم هاست و ما فرمی نداریم برای restApi
+app.use(bodyParser.json()) /// application/json 
 app.use(feedRoute)
 app.listen(3000, ()=>{
     console.log('run server on port 3000')
