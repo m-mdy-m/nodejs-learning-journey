9a253b802b216690e06230e9c3a0e3b2ac2f61ef
mahdi <mahdimamashli1383@gmail.com>
2024-01-12 17:27:05 +0330

    Add mock data for posts.

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
-exports.getPosts = (req,res,nxt)=>{
-    res.json({})
-}
+exports.getPosts = (req, res, nxt) => {
+  res
+    .status(200)
+    .json({ posts: [{ title: "POSTS", content: "THIS IS CONTENT POSTS" }] });
+};
