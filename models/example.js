d139d005a0a868cbe3976bc3bef23114b32ea23c
mahdi <mahdimamashli1383@gmail.com>
2024-01-14 17:33:26 +0330

    Changed the type of `_creator` field from `String` to `Object`.

---
+++ models/example.js
@@ -0,0 +1,20 @@
+const { Schema , default:mongoose } = require('mongoose')
+
+
+const example = new Schema({
+    _one:{
+        type: String,
+        required : true,
+    },
+    _two:{
+        type: String,
+        required : true,
+    },
+    _creator:{
+        type : Object,
+        required : String,   // یعنی این به صورت ابجکته و همه رشته هستند
+    },
+}, {timeseries : true})
+
+
+module.exports = mongoose.model("example" , example)
